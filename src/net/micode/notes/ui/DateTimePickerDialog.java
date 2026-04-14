373f84d0fcad76da9ad77f3f18ce20e0e5ef394e
Methylbenzene23 <2320499203@qq.com>
2026-04-15 00:10:55 +0800

    DateTimePickerDialog

--- src/net/micode/notes/ui/DateTimePickerDialog.java
+++ src/net/micode/notes/ui/DateTimePickerDialog.java
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+/*
+ * 设置提醒时间的对话框
+ */
 package net.micode.notes.ui;
 
 import java.util.Calendar;
@@ -31,22 +34,30 @@ import android.text.format.DateUtils;
 
 public class DateTimePickerDialog extends AlertDialog implements OnClickListener {
 
+    //创建calendar对象记录数据方便操作
     private Calendar mDate = Calendar.getInstance();
+    //记录是否为24h制
     private boolean mIs24HourView;
+    //时间日期滚动控件
     private OnDateTimeSetListener mOnDateTimeSetListener;
+    //设置提醒日期控件
     private DateTimePicker mDateTimePicker;
 
     public interface OnDateTimeSetListener {
         void OnDateTimeSet(AlertDialog dialog, long date);
     }
 
+    //传入时间的构造方法，进行初始化
     public DateTimePickerDialog(Context context, long date) {
+        // 初始化，将DateTimePicker对象放入对话框
         super(context);
         mDateTimePicker = new DateTimePicker(context);
         setView(mDateTimePicker);
+        // 设置日期变化时的监听器
         mDateTimePicker.setOnDateTimeChangedListener(new OnDateTimeChangedListener() {
             public void onDateTimeChanged(DateTimePicker view, int year, int month,
                     int dayOfMonth, int hourOfDay, int minute) {
+                //修改相关数据，更新对话框标题
                 mDate.set(Calendar.YEAR, year);
                 mDate.set(Calendar.MONTH, month);
                 mDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -55,32 +66,43 @@ public class DateTimePickerDialog extends AlertDialog implements OnClickListener
                 updateTitle(mDate.getTimeInMillis());
             }
         });
+        // 数据初始化，同步状态
         mDate.setTimeInMillis(date);
         mDate.set(Calendar.SECOND, 0);
         mDateTimePicker.setCurrentDate(mDate.getTimeInMillis());
+        // 加入设置按钮和取消按钮
         setButton(context.getString(R.string.datetime_dialog_ok), this);
         setButton2(context.getString(R.string.datetime_dialog_cancel), (OnClickListener)null);
         set24HourView(DateFormat.is24HourFormat(this.getContext()));
+        // 更新标题
         updateTitle(mDate.getTimeInMillis());
     }
 
+    // 设置是否为24h制
     public void set24HourView(boolean is24HourView) {
         mIs24HourView = is24HourView;
     }
 
+    // 回调
     public void setOnDateTimeSetListener(OnDateTimeSetListener callBack) {
         mOnDateTimeSetListener = callBack;
     }
 
+    // 更新标题
     private void updateTitle(long date) {
+        // 组合标志位
         int flag =
             DateUtils.FORMAT_SHOW_YEAR |
             DateUtils.FORMAT_SHOW_DATE |
             DateUtils.FORMAT_SHOW_TIME;
+        // 根据是否为24h制修改标题时间显示格式，推测是编写失误，修改如下
+        // flag |= mIs24HourView ? DateUtils.FORMAT_24HOUR : DateUtils.FORMAT_12HOUR;
         flag |= mIs24HourView ? DateUtils.FORMAT_24HOUR : DateUtils.FORMAT_24HOUR;
+        // 设置标题
         setTitle(DateUtils.formatDateTime(this.getContext(), date, flag));
     }
 
+    // 点击设置按钮触发的回调函数
     public void onClick(DialogInterface arg0, int arg1) {
         if (mOnDateTimeSetListener != null) {
             mOnDateTimeSetListener.OnDateTimeSet(this, mDate.getTimeInMillis());
