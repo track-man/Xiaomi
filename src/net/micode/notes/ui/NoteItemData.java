b9c8e734c9352acc73fecd7274b0b04c07397168
Methylbenzene23 <2320499203@qq.com>
2026-04-15 18:50:35 +0800

    NoteItemData

--- src/net/micode/notes/ui/NoteItemData.java
+++ src/net/micode/notes/ui/NoteItemData.java
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+/*
+ * 处理便签项数据
+ */
 package net.micode.notes.ui;
 
 import android.content.Context;
@@ -27,6 +30,7 @@ import net.micode.notes.tool.DataUtils;
 
 
 public class NoteItemData {
+    // 投影
     static final String [] PROJECTION = new String [] {
         NoteColumns.ID,
         NoteColumns.ALERTED_DATE,
@@ -42,6 +46,7 @@ public class NoteItemData {
         NoteColumns.WIDGET_TYPE,
     };
 
+    // 下标常量
     private static final int ID_COLUMN                    = 0;
     private static final int ALERTED_DATE_COLUMN          = 1;
     private static final int BG_COLOR_ID_COLUMN           = 2;
@@ -55,27 +60,51 @@ public class NoteItemData {
     private static final int WIDGET_ID_COLUMN             = 10;
     private static final int WIDGET_TYPE_COLUMN           = 11;
 
+    // 笔记数据
+    // 笔记id
     private long mId;
+    // 提醒时间
     private long mAlertDate;
+    // 背景颜色
     private int mBgColorId;
+    // 创建日期
     private long mCreatedDate;
+    // 是否带有附件
     private boolean mHasAttachment;
+    // 修改日期
     private long mModifiedDate;
+    // 如果是文件夹，则记录里面笔记的数量
     private int mNotesCount;
+    // 父节点id，存储上级文件夹id
     private long mParentId;
+    // 笔记摘要
     private String mSnippet;
+    // 笔记类型
     private int mType;
+    // 关联的组件id
     private int mWidgetId;
+    // 关联的组件类型
     private int mWidgetType;
+
+    // 通话笔记相关
+    // 姓名
     private String mName;
+    // 电话号码
     private String mPhoneNumber;
 
+    // ui相关属性
+    // 是否是列表末项
     private boolean mIsLastItem;
+    // 是否是列表首项
     private boolean mIsFirstItem;
+    // 是否是列表中唯一的项
     private boolean mIsOnlyOneItem;
+    // 如果是文件夹，后面是否只有一条笔记
     private boolean mIsOneNoteFollowingFolder;
+    // 如果是文件夹，后面是否有多条笔记
     private boolean mIsMultiNotesFollowingFolder;
 
+    // 构造方法，通过游标查询到的信息进行初始化
     public NoteItemData(Context context, Cursor cursor) {
         mId = cursor.getLong(ID_COLUMN);
         mAlertDate = cursor.getLong(ALERTED_DATE_COLUMN);
@@ -109,6 +138,7 @@ public class NoteItemData {
         checkPostion(cursor);
     }
 
+    // ui渲染属性判断
     private void checkPostion(Cursor cursor) {
         mIsLastItem = cursor.isLast() ? true : false;
         mIsFirstItem = cursor.isFirst() ? true : false;
@@ -116,13 +146,19 @@ public class NoteItemData {
         mIsMultiNotesFollowingFolder = false;
         mIsOneNoteFollowingFolder = false;
 
+        // 如果不是列表首项，并且是普通笔记
         if (mType == Notes.TYPE_NOTE && !mIsFirstItem) {
+            // 记录游标当前位置
             int position = cursor.getPosition();
+            // 游标移动到前一项
             if (cursor.moveToPrevious()) {
+                // 前一项是文件夹或系统文件夹
                 if (cursor.getInt(TYPE_COLUMN) == Notes.TYPE_FOLDER
                         || cursor.getInt(TYPE_COLUMN) == Notes.TYPE_SYSTEM) {
+                    // 文件夹后有多个笔记
                     if (cursor.getCount() > (position + 1)) {
                         mIsMultiNotesFollowingFolder = true;
+                    // 文件夹后只有一个笔记
                     } else {
                         mIsOneNoteFollowingFolder = true;
                     }
@@ -134,6 +170,7 @@ public class NoteItemData {
         }
     }
 
+    // 一大堆getter方法
     public boolean isOneFollowingFolder() {
         return mIsOneNoteFollowingFolder;
     }
@@ -210,14 +247,17 @@ public class NoteItemData {
         return mSnippet;
     }
 
+    // 有无提醒
     public boolean hasAlert() {
         return (mAlertDate > 0);
     }
 
+    // 是否为通话笔记
     public boolean isCallRecord() {
         return (mParentId == Notes.ID_CALL_RECORD_FOLDER && !TextUtils.isEmpty(mPhoneNumber));
     }
 
+    // 得到笔记类型
     public static int getNoteType(Cursor cursor) {
         return cursor.getInt(TYPE_COLUMN);
     }
